9ef1c04fbb2b8b661443a6e887e174e8ffa28b01
David Marshall <dlmarshall3@protonmail.com>
2023-10-07 21:59:19 -0500

    github pages changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dlmarshall3.github.io',
+    base: '/correlation-one-astro'
+});
